9657e4d0e6141d6025fce8327ccc6e9c455ce3a5
David Luciano <65189280+DaveMaia@users.noreply.github.com>
2024-08-31 12:07:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,70 @@
+
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-    function exibirTextoNaTela(tag, texto){
-        let campo = document.querySelector(tag);
-        campo.innerHTML = texto;
-        responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.1});
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
-exibirTextoNaTela('h1', 'Jogo do Nº Secreto');
-exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
+exibirMensagemInicial();
 
-function verificarChute(){
+function verificarChute() {
     let chute = document.querySelector('input').value;
-
+    
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled')
-    } else if (chute > numeroSecreto) {
-        exibirTextoNaTela('p', 'O número secreto é menor');
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        exibirTextoNaTela('p', 'O número secreto é maior');
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
     }
-    tentativas++
-    limparCampo();
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * numeroLimite);
-    let quantidadeDeNumerosNaLista = listaDeNumerosSorteados.length
-    
-    if (quantidadeDeNumerosNaLista == numeroLimite) {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
         listaDeNumerosSorteados = [];
     }
-
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
     } else {
-        listaDeNumerosSorteados.push()
-        return numeroEscolhido
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados)
+        return numeroEscolhido;
     }
 }
 
-function limparCampo(){
+function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
 
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
 
 /* 
 //Crie uma função que calcule o índice de massa corporal(IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
